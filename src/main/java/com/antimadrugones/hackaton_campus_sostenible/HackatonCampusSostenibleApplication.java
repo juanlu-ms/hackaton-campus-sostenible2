fe71d00d5a7a95c01b7adeb37a45ca2fcfd3d1e3
juanlu-ms <juanlums11@gmail.com>
2025-05-28 10:11:49 +0200

    Commit inicial

---
+++ src/main/java/com/antimadrugones/hackaton_campus_sostenible/HackatonCampusSostenibleApplication.java
@@ -0,0 +1,13 @@
+package com.antimadrugones.hackaton_campus_sostenible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonCampusSostenibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonCampusSostenibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antimadrugones/hackaton_campus_sostenible/HackatonCampusSostenibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antimadrugones.hackaton_campus_sostenible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonCampusSostenibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
